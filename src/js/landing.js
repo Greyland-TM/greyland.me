53c32949d9ae1a0d1da17857e8690c5d32a1a604
Greyland-TM <greyland.official@gmail.com>
2021-01-17 18:08:08 -0800

    main

--- src/js/landing.js
+++ src/js/landing.js
@@ -1,11 +1,11 @@
 import React from 'react'
-import me from '../media/myImage.jpg';
+// import me from '../media/myImage.jpg';
 
 const Landing = () => (
     <div className="landing">
         <div className="land">
                 <h1 class="land__me">Greyland T. Miller</h1>
-                <h2 class="land__skills">HTMl/SCSS | JavaScript / React | UX / UI design | Microsoft VBA</h2>\
+                <h2 class="land__skills">Finally a developer who cares about your buisness.</h2>
         </div>
     </div>
 );
--- src/js/skills.js
+++ src/js/skills.js
@@ -1,25 +1,46 @@
 import React from 'react'
+import { DiJavascript } from "react-icons/di";
+import { SiMicrosoftexcel } from "react-icons/si";
+import { IoBriefcase } from "react-icons/io5";
+import { GiColdHeart } from "react-icons/gi";
 
 const Skills = () => (
     <div className="skills">
-        <div className="skills__javascript">
-            <h2 className="skills__javascript--text">Javascript</h2>
+        
+        <div className="skills__item">
+            <div className="skills__title">
+                <DiJavascript className='skills__icon'/>
+                <h2 className="skills__head">Javascript</h2>
+            </div>
+            <p className="skills__text">I use the most modern and advanced tools to make sure your website is fast and responsive.</p>
+            <button className="btn skills__btn">Tell me more &rarr; </button>
         </div>
 
-        <div className="skills__html">
-            <h2 className="skills__html--text">HTML / SCSS</h2>
+        <div className="skills__item">
+            <div className="skills__title">
+                <GiColdHeart className='skills__icon'/>
+                <h2 className="skills__head">UX / UI design</h2>
+            </div>
+            <p className="skills__text">You found someone who cares about the way your application looks and feels.</p>
+            <button className="btn skills__btn">Tell me more &rarr; </button>
         </div>
 
-        <div className="skills__design">
-            <h2 className="skills__design--text">UX / UI design</h2>
+        <div className="skills__item">
+            <div className="skills__title">
+                <SiMicrosoftexcel className='skills__icon'/>
+                <h2 className="skills__head">Microsoft VBA</h2>
+            </div>
+            <p className="skills__text">I create custom microsoft apllications for tracking progress, employees, or supplys.</p>
+            <button className="btn skills__btn">Tell me more &rarr; </button>
         </div>
 
-        <div className="skills__microsoft">
-            <h2 className="skills__microsoft--text">Microsoft VBA</h2>
-        </div>
-
-        <div className="skills__buisness">
-            <h2 className="skills__buisness--text">Buisness automation</h2>
+        <div className="skills__item">
+            <div className="skills__title">
+                <IoBriefcase className='skills__icon'/>
+                <h2 className="skills__head">Buisness automation</h2>
+            </div>
+            <p className="skills__text">Dont waste time on repetitive tasks. I'll develope solutions to save your company time.</p>
+            <button className="btn skills__btn">Tell me more &rarr; </button>
         </div>
     </div>
 );
